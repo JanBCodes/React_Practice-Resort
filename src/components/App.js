3cca31b74196a550b4c1513c506dd32d0ba93e89
JanBCodes <janiellebacchus@gmail.com>
2021-06-09 03:54:58 -0400

    Created Directory

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+// Root Component
+// Creating a Component 
+
+import React from 'react'
+import css from '../assets/css/App.css'
+
+const App = () => {
+  return (
+    <div>
+      Testing
+    </div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Entry point File: 
+// Calling/Import Your Components
+
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
